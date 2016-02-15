1b4bb7a0140f8fef4fe0870f7839a8c400d30996
chenh2 <chenh1@hotmail.com>
2016-02-14 17:39:01 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Fun '16</title>
+	 <meta name="viewport" content="initial-scale=1.0">
+	<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<div class="main-container">
+		<div class="main-content">
+		
+		<div class="v-text">Happy Valentine's Day</div>
+
+		<div class="heart-container">
+			<div class="triangle-row triangle-row-1">
+				<div class="triangle-container">
+					<div class="triangle triangle-1"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-2"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-3"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-4"></div>
+				</div>
+			</div>
+
+			<div class="triangle-row triangle-row-2">
+				<div class="triangle-container">
+					<div class="triangle triangle-5"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-6"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-6-1"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-6-2"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-7"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-8"></div>
+				</div>
+			</div>
+
+			<div class="triangle-row triangle-row-3">
+				<div class="triangle-container">
+					<div class="triangle triangle-9"></div>
+				</div>
+				<div class="triangle-container">
+					<div class="triangle triangle-0"></div>
+				</div>
+			</div>
+		</div>
+			
+		<div class="v-text sub">
+			Roses are red, violets are blue, Bear love cub...thanks <3
+			Bear think web thing last longer than flowers...hehe
+		</div>
+		<div class="v-text">-Bear</div>
+
+		</div>
+	</div>
+
+</body>
+
+</html>
